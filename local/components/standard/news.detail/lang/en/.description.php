078e7e15c47762bb3a7a9fd1508f6466d4c11899
Alex <esasser@yandex.u>
2019-08-03 21:01:55 +0300

    refactor standard new.detail component

---
+++ local/components/standard/news.detail/lang/en/.description.php
@@ -0,0 +1,5 @@
+<?php
+
+$MESS['DESC_DETAIL']       =   'News details';
+$MESS['DESC_DETAIL_DESC']  =   'Shows the news details';
+$MESS['DESC_NEWS']         =   'News';
---
+++ local/components/standard/news.detail/lang/ru/.description.php
@@ -0,0 +1,5 @@
+<?php
+
+$MESS['DESC_DETAIL']        =   'Новость детально';
+$MESS['DESC_DETAIL_DESC']   =   'Показывает новость подробно';
+$MESS['DESC_NEWS']          =   'Статьи и новости';
