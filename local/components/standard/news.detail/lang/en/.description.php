c1367c591ef149738df80df998a5c99f61df4747
Alex <esasser@yandex.u>
2019-08-26 16:58:42 +0300

    remove standart bitrix component news

--- local/components/standard/news.detail/lang/en/.description.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$MESS['DESC_DETAIL']       =   'News details';
-$MESS['DESC_DETAIL_DESC']  =   'Shows the news details';
-$MESS['DESC_NEWS']         =   'News';
--- local/components/standard/news.detail/lang/ru/.description.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$MESS['DESC_DETAIL']        =   'Новость детально';
-$MESS['DESC_DETAIL_DESC']   =   'Показывает новость подробно';
-$MESS['DESC_NEWS']          =   'Статьи и новости';
